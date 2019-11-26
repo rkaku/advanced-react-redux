a2e5b332ca535e6e578e724425f8a86d8fd3b8e5
rkaku <greenapple728@icloud.com>
2019-11-26 12:59:25 +0900

    testing

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList'
+
+function App () {
+  return (
+    <div>
+      <CommentBox />
+      <CommentList />
+    </div>
+  );
+}
+
+export default App;
